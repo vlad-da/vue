48aaffc85d40e68d4160136efa869560435384d9
Vladislav Vlasenko <vlad-vlasenko@mail.ru>
2021-08-23 13:52:13 +0300

    created list

--- to-do-list/index.js
+++ to-do-list/index.js
@@ -1,7 +1,26 @@
 'use strict'
-var app = new Vue({
+const app = new Vue({
     el: '#app',
     data: {
         message: 'helo world'
     }
 });
+
+const app3 = new Vue({
+    el: '#app-3',
+    data: {
+        seen: true
+    }
+});
+
+const app4 = new Vue({
+    el: '#app-4',
+    data: {
+        todos: [
+            {text: 'how'},
+            {text: 'are'},
+            {text: 'you'},
+            {text: '?'}
+        ]
+    }
+})
